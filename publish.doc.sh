935d029b84190683bea190da85dc7b37c85ab1b5
bemowski <bemowski@gmail.com>
2011-04-14 13:38:20 +0000

    final 1.1.1 updates

--- publish.doc.sh
+++ publish.doc.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 export VERSION=$1
+
+if [ "X${VERSION}" == "X" ]
+then
+	echo "Usage:  $0 [version]"
+	exit 1
+fi
+
 export DOCDIR=build/docs
 
 export SVNURL=https://eproperties.googlecode.com/svn/docs/$VERSION
@@ -11,17 +18,23 @@ sleep 5
 cd $DOCDIR
 echo Changed to $PWD
 
-echo Making remote dir.
+echo Making remote dir, you have 5s to stop
+echo svn mkdir $SVNURL -m "" 
+sleep 5
 svn mkdir $SVNURL -m "" 
 
-echo Importing
+sleep 5
+echo Importing files below, you have 5s to stop
+ls
+sleep 5
 svn import . $SVNURL -m ""
 
 echo Checking out for propset
+rm -rf /tmp/epdocs
 
-svn co $SVNURL foo
+svn co $SVNURL /tmp/epdocs
 
-cd foo
+cd /tmp/epdocs
 
 echo calling propsets
 for file in `find . -name "*.html"`; do svn propset svn:mime-type text/html $file; done
@@ -33,9 +46,8 @@ echo commiting
 svn commit -m ""
 
 echo cleaning up
-cd ..
-rm -rf foo
 cd -
+rm -rf /tmp/epdocs
 
 echo "Docs version $VERSION published to SVN."
 echo "at url $SVNURL"
